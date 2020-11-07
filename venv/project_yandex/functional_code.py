bb2e83e1e17f2e0976502101f272e208345a433d
Niyaz <nijaz.shaidullin@yandex.ru>
2020-11-07 18:23:00 +0300

    uploading database experience

--- venv/project_yandex/functional_code.py
+++ venv/project_yandex/functional_code.py
@@ -10,11 +10,13 @@ from PyQt5.QtCore import *
 from PyQt5.Qt import pyqtSignal
 import sqlite3
 
-class MyWidget(QMainWindow):
+class Function_draw(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('project.ui', self)  # делал конкретный путь, можно заменить
-        self.pushButton.clicked.connect(self.painting)
+        self.drawButton.clicked.connect(self.painting)
+        self.backButton.clicked.connect(self.exit)
+        self.mainMenu = None
 
     def painting(self):
         plt.ioff()
@@ -22,20 +24,29 @@ class MyWidget(QMainWindow):
         ax.set_title('Graph:', fontsize=12)
         plt.axis([-10, 10, -10, 10])
         x = np.arange(-10, 10, 0.01)
-        y = eval(self.lineEdit.text())  # планирую сделать безопасный eval, с проверкой на содержимое lineEdit'а
-        ax.grid(color='grey',  # цвет линий # Сетка графика
-                linewidth=1,  # толщина # Сетка графика
-                linestyle='-')  # начертание # Сетка графика
-        plt.plot(x, y, "g-")  # Построение графика
-        # X - axis
-        ax.axhline(y=0, color='k')
-        # Y - axis
-        ax.axvline(x=0, color='k')
-        plt.savefig('abc.png')
-        image = Image.open('abc.png')
-        new_image = image.resize((491, 491))
-        new_image.save('a1b1c1.png')
-        self.graphic_label.setPixmap(QPixmap('a1b1c1.png'))
+        if self.lineEdit.text() == '':
+            self.error_Label.setText('Write a func')
+        else:
+            y = eval(self.lineEdit.text())  # планирую сделать безопасный eval, с проверкой на содержимое lineEdit'а
+            ax.grid(color='grey',  # цвет линий # Сетка графика
+                    linewidth=1,  # толщина # Сетка графика
+                    linestyle='-')  # начертание # Сетка графика
+            plt.plot(x, y, "g-")  # Построение графика
+            # X - axis
+            ax.axhline(y=0, color='k')
+            # Y - axis
+            ax.axvline(x=0, color='k')
+            plt.savefig('abc.png')
+            image = Image.open('abc.png')
+            new_image = image.resize((491, 491))
+            new_image.save('a1b1c1.png')
+            self.graphic_label.setPixmap(QPixmap('a1b1c1.png'))
+    def exit(self):
+        self.close()
+        self.mainMenu.show()
+    def addMainMenu(self, mainMenu):
+        self.mainMenu = mainMenu
+
 
 
 class Autorization(QWidget):
@@ -44,9 +55,13 @@ class Autorization(QWidget):
         uic.loadUi('authorization.ui', self)
         self.mainMenu = None
         self.check = False
-
         self.registerButton.clicked.connect(self.user_registration)
         self.loginButton.clicked.connect(self.user_login)
+        self.count = 0
+        self.changebutton.clicked.connect(self.pass_change)
+        self.changeLabel.hide()
+        self.lineEdit.hide()
+        self.changebutton.hide()
 
 
     def user_login(self):
@@ -69,12 +84,22 @@ class Autorization(QWidget):
             #db.commit()
             #print('Your account is registered')
         else:
-            sql.execute(f"""SELECT LOGIN FROM users WHERE login = '{user_password}'""")
+            sql.execute(f"""SELECT PASSWORD FROM users WHERE password = '{user_password}' AND login = '{user_login}'""")
             if sql.fetchone() is None:
                 self.warningLabel.setText('Wrong password')
+                self.count += 1
+                if self.count < 3:
+                    self.changeLabel.hide()
+                    self.lineEdit.hide()
+                    self.changebutton.hide()
+                elif self.count >= 3:
+                    self.changeLabel.show()
+                    self.lineEdit.show()
+                    self.changebutton.show()
             else:
                 self.check = True
                 self.mainMenu.setEnabled(True)
+                self.count = 0
                 self.close()
     def user_registration(self):
         db = sqlite3.connect('accounts.sqlite')
@@ -107,6 +132,17 @@ class Autorization(QWidget):
                 self.close()
             else:
                 self.warningLabel.setText('User is already registered')
+
+    #def pass_change(self):
+        #if len(self.lineEdit.text()) > 4:
+            #sql.execute(f"""UPDATE users SET password = '{self.lineEdit.text()}' WHERE login = '{self.nickEdit.text()}'""")
+            #db.commit()
+            #self.check = True
+            #self.mainMenu.setEnabled(True)
+            #self.close()
+        #else:
+            #self.warningLabel.setText('Wrong password')
+
     def checkAutorization(self):
         return self.check
 
@@ -114,12 +150,40 @@ class Autorization(QWidget):
         self.mainMenu = menu
 
 
+class MainMenu(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('mainMenu.ui', self)
+
+        self.function_draw = None
+
+        self.startButton.clicked.connect(self.startFunc)
+        self.exitButton.clicked.connect(self.exit)
+
+    def exit(self):
+        sys.exit()
+
+    def startFunc(self):
+        self.close()
+        self.function_draw.show()
+        self.function_draw.setEnabled(True)
+
+    def addFunc(self, function_draw):
+        self.function_draw = function_draw
+
+
 class FuncPunk(QObject):
     def __init__(self):
         super().__init__()
-        self.mainMenu = MyWidget()
+        self.fuction_draw = Function_draw()
+        self.fuction_draw.hide()
+        self.fuction_draw.setEnabled(False)
+
+        self.mainMenu = MainMenu()
+        self.mainMenu.addFunc(self.fuction_draw)
         self.mainMenu.show()
         self.mainMenu.setEnabled(False)
+        self.fuction_draw.addMainMenu(self.mainMenu)
 
 
         self.autorization = Autorization()
